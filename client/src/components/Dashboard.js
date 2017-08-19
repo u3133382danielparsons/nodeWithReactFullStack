f0eabd95d4f78b5bf546447bdd76f5e4dd7ba727
Daniel Parsons <danielparsons3133382@gmail.com>
2017-08-19 17:50:30 +1000

    Added a Dashboard

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Dashboard!</h1>
+      This is a Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -13,7 +13,7 @@ class Payments extends Component {
         token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
-        <button className="btn right" style={{ margin: '8px' }}>
+        <button className="btn right" style={{ marginTop: '8px' }}>
           Donations
         </button>
       </StripeCheckout>
