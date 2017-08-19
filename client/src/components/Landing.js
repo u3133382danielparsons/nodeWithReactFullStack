683e9859b0702c0b3e6b71927c0d166b55cbb762
Daniel Parsons <danielparsons3133382@gmail.com>
2017-08-19 19:15:55 +1000

    More work done on the Landing Page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import nl2br from 'react-newline-to-break';
 
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Family History Seeker!</h1>
-      Search online repositories for your family history
+      <div>
+        <h2>Family History Seeker!</h2>
+      </div>
+      <h4>Search Online Family History Repositories</h4>
+      <div className="download-section" />
     </div>
   );
 };
